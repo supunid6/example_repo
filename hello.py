1092d575c1c8be745a62820e12ef5d643d567a11
supunid6@gmail.com <supunith@ip-172-31-93-109.ec2.internal>
2020-05-15 19:09:25 +0000

    adding python

---
+++ hello.py
@@ -0,0 +1,16 @@
+# This is my first python program
+
+name="Raj"
+age=22
+
+brothername="Vijay"
+brotherage=26
+
+agedifference=age-brotherage
+difference=abs(agedifference)
+
+print("My name is: ", name)
+print("My age is: ", age)
+print("My Brothers Name is:", brothername)
+print("My Brothers Age is:", brotherage)
+print("we were born", difference,"years apart")
